f5c7797e675b0bb58b2810a744cf634fff0b2acc
Teken <timu.rev006@gamil.com>
2026-02-21 23:33:29 +0900

    update

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
+    
