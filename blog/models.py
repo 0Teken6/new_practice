59ade0de5c9d83153aa7599deb36ccca01abc4fe
Teken <timu.rev006@gamil.com>
2026-02-27 13:50:20 +0900

    update

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils import timezone
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -12,4 +13,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
     
